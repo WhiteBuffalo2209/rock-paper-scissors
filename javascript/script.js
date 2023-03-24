57ac2fc1e61c33c240f34cc24698efa00389d93e
WhiteBuffalo2209 <louie.manalo@rakuten.com>
2023-03-24 12:50:07 +0900

    Change the return value of victor to a single word

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,30 @@
-let gish;
-gish = 'hello bagel man';
 let playerSelection;
 let computerSelection;
 playerSelection = prompt('rock, paper, or scissors');
+playerSelection = playerSelection.toLowerCase();
 computerSelection = getComputerChoice();
+
 
 function playRound(playerSelection, computerSelection) {
     let victor;
-    playerSelection = playerSelection.toLowerCase();
     if ((playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'rock' && computerSelection === 'scissors')) {
-        victor = `Player selects ${playerSelection} and Computer selects ${computerSelection}, you win! Congratulations!`;
+        console.log(`Player selects ${playerSelection} and Computer selects ${computerSelection}, you win! Congratulations!`);
+        victor = 'player';
         return victor;
     }
     else if ((playerSelection === 'scissors' && computerSelection === 'rock') || (playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors')) {
-        victor = `Player selects ${playerSelection} and Computer selects ${computerSelection}, you lose.`;
+        console.log(`Player selects ${playerSelection} and Computer selects ${computerSelection}, you lose.`);
+        victor = 'computer';
+        return victor;
+    }
+    else if ((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
+        console.log(`Player selects ${playerSelection} and Computer selects ${computerSelection}, it's a draw.`);
+        victor = 'draw';
         return victor;
     }
     else {
-        victor = `Player selects ${playerSelection} and Computer selects ${computerSelection}, it's a draw`;
+        console.log(`Player selects ${playerSelection} and Computer selects ${computerSelection}, INVALID input.`);
+        victor = 'invalid';
         return victor;
     }
 
